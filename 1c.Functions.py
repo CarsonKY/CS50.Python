7c34191667d85bc009f2cecc1647ea8d286c6a49
Carson Hall <carsonh@gmail.com>
2023-11-29 15:19:51 -0500

    Functions start

---
+++ 1c.Functions.py
@@ -0,0 +1,9 @@
+# Create function 
+def Hello(to):
+    # Line 4 does not print
+    print("Winston is cool and says hello to", to)
+name = input ("Who is this  ") ;
+Hello(name)
+Hello(name)
+
+# 1:33 in video
