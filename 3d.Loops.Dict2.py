29258568d692fbb0994bdae25c5dc88a2430181d
Carson Hall <carsonh@gmail.com>
2023-12-22 16:05:58 -0500

    Loops.Cont

---
+++ 3d.Loops.Dict2.py
@@ -0,0 +1,8 @@
+# creating a dictionary with multiple values
+
+dognames = [
+    {"name": "Albert", "owner": "Kramers", "type": "cat"},
+    {"name": "Mandy", "owner": "myself", "type": "corgi"},
+    {"name": "Winston", "owner": "myself", "type": "corgi"},
+    {"name": "Pookie", "owner": "Angell", "type": "mutt"}
+]
